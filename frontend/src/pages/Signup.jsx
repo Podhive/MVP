83488f72f2c359abee2f1f37fbf866e0da1a9bf3
Tanishq Vats <tanishqvats620@gmail.com>
2025-08-04 11:18:00 +0530

    Full MVP completed

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -176,7 +176,7 @@ const Signup = () => {
                     htmlFor="phone"
                     className="block text-sm font-medium text-gray-700 mb-2"
                   >
-                    Phone Number
+                    Whatsapp Number
                   </label>
                   <div className="relative">
                     <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
